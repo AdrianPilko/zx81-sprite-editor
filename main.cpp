3d134ce4a5f71465130ef399c24e1e5e81a38353
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2025-04-19 10:44:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -66,7 +66,15 @@
 // --                   |   oo
 //====================================================
 
+// this vecotr stores the pattern to match, and linearly so pattern 3 form table able becomes "oo--"
+// the second row are the inverse patterns, 22 in total
+const std::vector<std::string> patterns = {"----","o---","-o--","oo--","--o-","o-o-","-oo-","ooo-","****","--**","**--",
+                                           "oooo","-ooo","o-oo","--oo","oo-o","-o-o","o--o","---o","@@@@","oo@@","@@oo"};
+// these are the codes to match the patterns as will be written to the output file
+const std::vector<std::string> patternHexCodes = {"$00","$01","$02","$03","$04","$05","$06","$07","$08","$09","$0A",
+                                                  "$80","$81","$82","$83","$84","$85","$86","$87","$88","$89","$90"};
 
+                                            
 int32_t parseInput(const std::string & inFileName, const std::string & outFileName)
 {
     std::fstream iStream(inFileName, std::ios::in);
@@ -77,33 +85,67 @@ int32_t parseInput(const std::string & inFileName, const std::string & outFileNa
        std::cerr << "Error: Could not open the file(s)!" << std::endl;
        return EXIT_FAILURE;
     }
-    
+
+    oStream << "spriteData" << std::endl;
+    oStream << "   DB ";   
     std::string line;
     std::vector<std::string> fileAsStrVec;
-
+    int32_t lastLineLen = 0;
+    int32_t currentLineLen = 0;
     while (std::getline(iStream, line)) 
     {
-       fileAsStrVec.push_back(line);
-    }
-    for (auto& elem:fileAsStrVec)
-    {
-        for (char ch : elem)
+        currentLineLen = line.size();
+        // check all lines are the same length and have to be even number
+        // to maintain the 2x2 block valid size
+        if ((currentLineLen != lastLineLen && lastLineLen != 0) || (currentLineLen % 2 != 0))
         {
-            if (ch == 'o')
+            std::cout << "ERROR: Lines not all even length" << std::endl;
+            return EXIT_FAILURE;
+        }
+        lastLineLen = currentLineLen;
+        fileAsStrVec.push_back(line);
+    }
+    // the number of lines also has to be even
+    if (fileAsStrVec.size() % 2 != 0)
+    {
+        std::cout << "ERROR: number of lines is not even" << fileAsStrVec.size() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // need to check each 2x2 block for the pixel type, the -1 is to go to penulimate row in outer loop
+    // as the nested loop will handle this
+    int32_t linearIndex = 0;
+    int32_t resultIndex = 0;
+    std::string linearVersion = "    ";
+    bool found = false;
+    
+    for (auto row = 0; row < fileAsStrVec.size() - 1; row++)
+    {   
+        for (auto columnOuter = 0; columnOuter < fileAsStrVec[row].size() - 1; columnOuter++)
+        {
+            for (auto col = 0; col < 2; col++)
             {
-                std::cout << "PIXEL ";
+                linearVersion[linearIndex++] = fileAsStrVec[row][col+columnOuter];
             }
-            else if (ch == '-') 
+            if (linearIndex >= 4)
             {
-                std::cout << "BLANK ";
-            } 
-            else 
-            {
-                std::cout << "OTHER ";
+                linearIndex = 0;
+                for (size_t fIndex = 0; fIndex < patterns.size(); fIndex++)
+                {
+                    if (patterns[fIndex].find(linearVersion) != std::string::npos)
+                    {
+                        found = true;
+                        oStream << patternHexCodes.at(fIndex) << ",";
+                    } 
+                }
+                std::string linearVersion = "    ";
             }
         }
-        std::cout << std::endl;
+
+        oStream <<std::endl;
+        oStream << "   DB ";
     }
+
     iStream.close();
     oStream.close();
     std::cout << "----" << std::endl;
@@ -122,7 +164,10 @@ int main(int argc, char * argv[])
         inFile = argv[1];
         outFile= argv[2];
         std::cout << "Using input file=" << inFile << " outputting to " << outFile << std::endl;
-        parseInput(inFile,outFile);
+        if (parseInput(inFile,outFile) != EXIT_SUCCESS)
+        {
+            std::cout << "Fault found in input file" << std::endl;
+        }
     }
     else
     {
